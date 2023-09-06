3ec76d7c46ae5e13fe1f33a086bf4a998bd9ab84
sadface2000 <144204179+sadface2000@users.noreply.github.com>
2023-09-06 14:09:48 +0800

    insertion sort

--- Project1.c
+++ Project1.c
@@ -12,7 +12,18 @@ include <studio.h>
 
 
 // insertionSort
-
+void insertionSort(int arr[], int left, int right){
+    for(int i=left+1; i<=right; i++){
+        int curr = arr[i];
+        int j = i - 1;
+        
+        while (j>=left && arr[j]>curr){
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=curr;
+    }
+}
 
 
 // main
