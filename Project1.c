293373477ed164503946da7a4e7795c707432706
POI <108055046+VoidWitch@users.noreply.github.com>
2023-09-06 17:40:31 +0800

    update

--- Project1.c
+++ Project1.c
@@ -1,13 +1,31 @@
 include <studio.h>
 //merge insertion hybrid
+/*
+
+*/
 
 
 
 // dataGeneration
+//max value of X is 10 million -> highest number of array elements is 10 million
+//array size, lowest 1000, increment by 500,000, max 10 million, 21 arrays
 
 
 
-// mergeSort
+// mergeSort + merge
+void MergeSort(int *arr, int l, int r){     //start and end index arr
+    if(l < r){    //if at least 2 elements
+        int mid = (l + r)/2;      //Calculate middle index
+
+        // Recursively sort two halves
+        MergeSort(arr, l, mid);
+        MergeSort(arr, mid + 1, r);
+
+        // Merge the sorted halves
+        merge(arr, l, mid, r);      //mid+1 starting index of right half
+    }
+}
+
 
 
 
@@ -27,3 +45,4 @@ void insertionSort(int arr[], int left, int right){
 
 
 // main
+//insertion nested within merge with if(s<n) branch -> S range from [0, 1e6] -> eg. 1, 10, 100, 1000...
