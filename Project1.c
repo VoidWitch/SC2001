7284773318bb8d17b1a5f0a4851e7090413a36b1
POI <108055046+VoidWitch@users.noreply.github.com>
2023-09-07 15:57:56 +0800

    update data gen and main

--- Project1.c
+++ Project1.c
@@ -6,38 +6,35 @@
 #define min 500000
 #define Smax 1000000
 
-int A[N];
-
 //functions
-void MergeSort(int *arr, int l, int r, int S);  //arr, start and end index, min subarr size)
-void merge(int *arr, int l, int m, int r);  //arr, left, mid, and right index
-void insertionSort(int arr[], int left, int right); //arr, left and right index
-void arrGeneration(int arr[], int s);  //arr generation of size s
+void MergeSort(int *arr, int l, int r, int S);
+void merge(int *arr, int l, int m, int r);
+void insertionSort(int arr[], int left, int right);
+int* arrGeneration(int s);
 
 int main(){
-    int i;
+    int i, input;
     double time1, timedif;
 
     //part (i)
-    int S = 100;    //fixed S hypothetical value
+    int S = 30;    //fixed S hypo value
 
     //generating arrays with 500k increment
-    for(i = min; i <= N; i+=min){
-        arrGeneration(A, i);   //generate arr 1, 2, ...n(20)
-        MergeSort(A, 0, i - 1, S); //start, end index of arr
+    do {
+        printf("Input array size: ");
+        scanf("%i", &input);
+        MergeSort(arrGeneration(input), 0, input - 1, S); //start, end index of arr
         time1 = clock(); //get initial time
         time1 = time1 / CLOCKS_PER_SEC;
         timedif = (clock()/CLOCKS_PER_SEC) - time1;  //get final time and get diff
         printf("%f ", timedif); //runtime for each arr
-    }
+    } while (input > 0);
 
     //part (ii)
-    int B[1000];    //fixed size input
-
+    int* B = arrGeneration(1000);   //fixed arr size
     //S range from [1, 10, 100...10e6] -> optimal technically arnd 10-100
-    arrGeneration(B, 1000);
     for(i = 1; i <= Smax; i *= 10){
-        mergeSort(B, 0, 999, i);
+        MergeSort(B, 0, 999, i);
         time1 = clock(); //get initial time
         time1 = time1 / CLOCKS_PER_SEC;
         timedif = (clock()/CLOCKS_PER_SEC) - time1;  //get final time and get diff
@@ -46,15 +43,19 @@ int main(){
 
 }
 
-// dataGeneration of 500,000 increment to 10 mil -> 20 arrs
-void arrGeneration(int arr[], int arr_size){   //1 arr is sufficient, index data override for increasing sizes
+// dataGeneration
+int* arrGeneration(int arr_size){   //1 arr is sufficient, index data override for increasing sizes
+    int *arr = (int*)malloc(arr_size*sizeof(int));
+
     for(int i = 0; i < arr_size; i++){
         arr[i] = (rand() % (arr_size - 1 + 1)) + 1;  //[rand()%(upper-lower+1)] + lower
     }
+    return arr;
 }
 
 
-// mergeSort + merge + insertion 
+
+// mergeSort + merge
 void MergeSort(int *arr, int l, int r, int S){     //start and end index arr
     if(l < r){    //if at least 2 elements
         int mid = (l + r)/2;      //Calculate middle index
@@ -67,6 +68,8 @@ void MergeSort(int *arr, int l, int r, int S){     //start and end index arr
         merge(arr, l, mid, r);      //mid+1 starting index of right half
     }
 }
+
+
 
 
 // insertionSort
