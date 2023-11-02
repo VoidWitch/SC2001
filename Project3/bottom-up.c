3874fd96f61d043ee84096e6caca158ab8ec0828
YM <110095482+YichenG170@users.noreply.github.com>
2023-11-02 15:46:56 +0800

    Update bottom-up.c
    remove the limit

--- Project3/bottom-up.c
+++ Project3/bottom-up.c
@@ -8,35 +8,41 @@ int max(int a, int b) { return (a > b) ? a : b; }
   
 // Returns the maximum value that 
 // can be put in a knapsack of capacity W 
-int knapSack(int W, int wt[], int val[], int n) 
-{ 
-    int i, w; 
-    int K[n + 1][W + 1]; 
-  
-    // Build table K[][] in bottom up manner 
-    for (i = 0; i <= n; i++) { 
-        for (w = 0; w <= W; w++) { 
-            if (i == 0 || w == 0) 
-                K[i][w] = 0; 
-            else if (wt[i - 1] <= w) 
-                K[i][w] = max(val[i - 1] 
-                                  + K[i - 1][w - wt[i - 1]], 
-                              K[i - 1][w]); 
-            else
-                K[i][w] = K[i - 1][w]; 
-        } 
-    } 
-  
-    return K[n][W]; 
-} 
+int knapSack(int W, int wt[], int val[], int n)
+{
+    int K[W + 1];
+    for (int i = 0; i <= W; i++)
+    {
+        K[i] = 0;
+    }
+
+    for (int w = 1; w <= W; w++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (wt[i] <= w)
+            {
+                K[w] = max(K[w], K[w - wt[i]] + val[i]);
+            }
+        }
+    }
+
+    return K[W];
+}
+
   
 // Driver Code 
 int main() 
 { 
-    int profit[] = { 60, 100, 120 }; 
-    int weight[] = { 10, 20, 30 }; 
-    int W = 50; 
-    int n = sizeof(profit) / sizeof(profit[0]); 
-    printf("%d", knapSack(W, weight, profit, n)); 
+    int profit_a[] = { 7, 6, 9 }; 
+    int weight_a[] = { 4, 6, 8 }; 
+    int profit_b[] = { 7, 6, 9 }; 
+    int weight_b[] = { 5, 6, 8 };    
+    int W = 14; 
+    int n = 3; 
+    printf("4.(a): ");
+    printf("%d\n", knapSack(W, weight_a, profit_a, n)); 
+    printf("4.(b): ");
+    printf("%d\n", knapSack(W, weight_b, profit_b, n)); 
     return 0; 
 }
